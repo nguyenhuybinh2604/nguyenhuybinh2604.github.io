49dc9f3cd7a1352cc277e3b98281778a6a7fb491
nguyenhuybinh2604 <nguyenhuybinh2604@gmail.com>
2023-03-25 17:49:35 +0700

    upload

---
+++ java/bai-tap-2/Main.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Scanner input = new Scanner(System.in);
+        // System.out.println("Mời bạn nhập tên hàng nhập kho:");
+        // String tenHang = input.nextLine();
+        // System.out.println("Mời bạn nhập ngày tháng năm sinh (yyyy/MM/dd):");
+        // LocalDate ngaySinh = LocalDate.parse(input.nextLine(), DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+        // System.out.println("Mời bạn nhập thời gian nhập hàng (yyyy/MM/dd HH:mm:ss):");
+        // LocalDateTime tGianNhapHang = LocalDateTime.parse(input.nextLine(),
+        //         DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+        // System.out.println("Mời bạn nhập thời gian (HH:mm:ss):");
+        // LocalTime tGian = LocalTime.parse(input.nextLine(), DateTimeFormatter.ofPattern("HH:mm:ss"));
+        // System.out.println("Tên hàng là: " + tenHang);
+        // System.out.println("Sinh nhật là: " + ngaySinh);
+        // System.out.println("Thời gian nhập hàng là: " + tGianNhapHang);
+        // System.out.println("Thời gian là: " + tGian);
+
+        // for (int i = 0; i < 5; i++) {
+        //     System.out.println(i);
+        // }
+        float v1 = 5.6f;
+        double v2 = 5.6d;
+        System.out.println(v1==v2);
+        // System.out.println(v1.equals(v2));
+    }
+}
---
+++ java/bai-tap-2/sample.java
@@ -0,0 +1,12 @@
+public class sample {
+    private int amount = 0;
+    public void exampleMethod() {
+            amount++;
+            System.out.println(amount);
+    }
+    public void anotherExampleMethod() {
+            int anotherAmount = amount + 4;
+            System.out.println(anotherAmount);
+    }
+    
+}
