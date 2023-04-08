438371e34eb6bcb47ca935abf7ce0b6555b36ca7
nguyenhuybinh2604 <126578373+nguyenhuybinh2604@users.noreply.github.com>
2023-04-09 00:06:10 +0700

    upload bai o lop

---
+++ java/thuc-hanh/20230408/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+import entity.Product;
+import handle.ProductHandle;
+import view.InputControl;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        System.out.println("Nhap so luong san pham:");
+
+        // Kiem soat gia tri la so khong am
+        do {
+            System.out.println("Nhap so luong (>=0): ");
+            // Yeu cau gia tri la so
+            while (!sc.hasNextInt()) {
+                System.out.println("Hay nhap mot so (>=0): ");
+                sc.nextLine();
+            }
+            n = Integer.parseInt(sc.nextLine());
+        } while (n < 0);
+
+        ProductHandle productHandle = new ProductHandle();
+        Product[] products = new Product[n];
+
+        for (int i = 0; i < n; i++) {
+            products[i] = productHandle.createProduct(sc, i);
+        }
+
+        InputControl inputControl = new InputControl();
+        inputControl.requestInput(sc, productHandle, products);
+
+    }
+}
---
+++ java/thuc-hanh/20230408/entity/Product.java
@@ -0,0 +1,65 @@
+package entity;
+
+public class Product {
+    public static int autoId;
+    private int id;
+    private String name;
+    private String description;
+    private int quantity;
+    private double price;
+    private String unit;
+
+    public Product() {
+        this.id = ++autoId;
+    };
+
+    public Product(String name, String description, int quantity, double price, String unit) {
+        this.id = ++autoId;
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+        this.price = price;
+        this.unit = unit;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public String getUnit() {
+        return this.unit;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " id='" + getId() + "'" +
+                ", name='" + getName() + "'" +
+                ", description='" + getDescription() + "'" +
+                ", quantity='" + getQuantity() + "'" +
+                ", price='" + getPrice() + "'" +
+                ", unit='" + getUnit() + "'" +
+                "}";
+    }
+
+}
