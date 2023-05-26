5f8d2b623716c894cc98b61bfeb6ea5ea77ecea8
nguyenhuybinh2604 <126578373+nguyenhuybinh2604@users.noreply.github.com>
2023-05-26 13:52:07 +0700

    upload algorithm

--- datastructurealgorithm/btvn/Main.java
+++ datastructurealgorithm/btvn/Main.java
@@ -4,10 +4,7 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-//        int[] myArray = {1, 1, 3, 3, 5, 6, 6, 8, 8};
-        int[] rec1 = {0, 0, 1, 1};
-        int[] rec2 = {2, 2, 3, 3};
-        System.out.println(solution.isRectangleOverlap_836(rec1,rec2));
+        int[] myArray = {3,1,3,1,0,1,2,1,0};
+        System.out.println(solution.canJump_55(myArray));
     }
-
 }
--- datastructurealgorithm/btvn/Solution.java
+++ datastructurealgorithm/btvn/Solution.java
@@ -1,5 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Solution {
 
@@ -60,10 +59,92 @@ public class Solution {
         return returnValue;
     }
 
+    //https://leetcode.com/problems/rectangle-overlap/submissions/957532834/
     public boolean isRectangleOverlap_836(int[] rec1, int[] rec2) {
-        boolean returnValue = false;
-        if ((rec1[0]>rec2[2] || rec1[2]>rec2[0]) &&
-                (rec1[1]>rec2[3] || rec1[3]>rec2[1])) returnValue=true;
+        ;
+        return ((rec1[2] > rec2[0] && rec2[2] > rec1[0]) &&
+                (rec1[3] > rec2[1] && rec2[3] > rec1[1]));
+    }
+
+    //https://leetcode.com/problems/majority-element/submissions/956968921/
+    public int majorityElement_169(int[] nums) {
+        int returnValue = 0;
+        int maxFreq = 0;
+        Map<Integer, Integer> result = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int count = result.getOrDefault(nums[i], 0);
+            result.put(nums[i], count + 1);
+            if (count + 1 > maxFreq) maxFreq = count + 1;
+        }
+        for (Map.Entry<Integer, Integer> entry : result.entrySet()) {
+            if (entry.getValue() == maxFreq) {
+                returnValue = entry.getKey();
+                break;
+            }
+        }
         return returnValue;
+    }
+
+    //  https://leetcode.com/problems/shuffle-the-array/submissions/956983719/
+    public int[] shuffle_1470(int[] nums, int n) {
+        int[] resultArray = new int[n * 2];
+        for (int i = 0; i < nums.length; i++) {
+            if (i < n) {
+                resultArray[i * 2] = nums[i];
+            } else {
+                resultArray[(i + 1 - n) * 2 - 1] = nums[i];
+            }
+        }
+        return resultArray;
+    }
+
+    //https://leetcode.com/problems/missing-number/submissions/957007756/
+    public int missingNumber_268(int[] nums) {
+        int returnValue = 0;
+        int[] countArray = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            countArray[nums[i]] = 1;
+        }
+        for (int i = 0; i < countArray.length; i++) {
+            if (countArray[i] == 0) {
+                returnValue = i;
+                break;
+            }
+        }
+        return returnValue;
+    }
+
+    //https://leetcode.com/problems/build-array-from-permutation/submissions/957009556/
+    public int[] buildArray_1920(int[] nums) {
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+
+    //https://leetcode.com/problems/jump-game/submissions/957432274/
+    public boolean canJump_55(int[] nums) {
+        int maxIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxIndex) return false;
+            maxIndex = Math.max(maxIndex, i + nums[i]);
+        }
+        return true;
+    }
+
+    //if using recursion
+    private boolean jump(int[] nums, int currentIndex) {
+        boolean result = false;
+        if (currentIndex >= nums.length - 1) return true;
+        else if (nums[currentIndex] == 0)
+            return false;
+        else {
+            for (int i = 1; i <= nums[currentIndex]; i++) {
+                result = jump(nums, currentIndex + i);
+                if (result) break;
+            }
+        }
+        return result;
     }
 }
