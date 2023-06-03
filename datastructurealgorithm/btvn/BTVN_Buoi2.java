b1b57edb3015a2b656eada3bbf0839006beae664
nguyenhuybinh2604 <126578373+nguyenhuybinh2604@users.noreply.github.com>
2023-06-03 10:03:38 +0700

    upload bai 02

--- datastructurealgorithm/btvn/BTVN_Buoi2.java
+++ datastructurealgorithm/btvn/BTVN_Buoi2.java
@@ -66,11 +66,6 @@ public class BTVN_Buoi2 {
         }
     }
 
-    public int longestMountain_845(int[] arr) {
-
-        return 0;
-    }
-
     //https://leetcode.com/problems/first-unique-character-in-a-string/submissions/960346654/
     public int firstUniqChar_387(String s) {
         for (int i = 0; i < s.length(); i++) {
@@ -82,4 +77,151 @@ public class BTVN_Buoi2 {
         }
         return -1;
     }
+
+    //https://leetcode.com/problems/split-a-string-in-balanced-strings/submissions/961607467/
+    public int balancedStringSplit_1221(String s) {
+        int countR = 0;
+        int countL = 0;
+        int countTotal = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'R') {
+                countR++;
+                if (countR == countL) {
+                    countTotal++;
+                    countL = 0;
+                    countR = 0;
+                }
+            } else {
+                countL++;
+                if (countR == countL) {
+                    countTotal++;
+                    countL = 0;
+                    countR = 0;
+                }
+            }
+        }
+        return countTotal;
+    }
+
+    // https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/submissions/961649906/
+    public boolean arrayStringsAreEqual_1662(String[] word1, String[] word2) {
+        int strIndexA = 0;
+        int charIndexA = 0;
+        int strIndexB = 0;
+        int charIndexB = 0;
+        while (strIndexA < word1.length && charIndexA < word1[strIndexA].length()
+                && strIndexB < word2.length && charIndexB < word2[strIndexB].length()) {
+            if (word1[strIndexA].charAt(charIndexA) == word2[strIndexB].charAt(charIndexB)) {
+                if (charIndexA == word1[strIndexA].length() - 1) {
+                    charIndexA = 0;
+                    strIndexA++;
+                } else charIndexA++;
+                if (charIndexB == word2[strIndexB].length() - 1) {
+                    charIndexB = 0;
+                    strIndexB++;
+                } else charIndexB++;
+                if (strIndexA == word1.length && charIndexA == 0
+                        && strIndexB == word2.length && charIndexB == 0) return true;
+            } else return false;
+        }
+        return false;
+    }
+
+    //https://leetcode.com/problems/longest-mountain-in-array/submissions/962553806/
+    public int longestMountain_845(int[] arr) {
+        int up = 0;
+        int down = 0;
+        int length = 0;
+        int maxLength = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == arr[i - 1]) {
+                up = 0;
+                down = 0;
+            } else if (arr[i] > arr[i - 1]) {
+                up++;
+                if (i < arr.length - 1 && arr[i] > arr[i + 1]) {
+                    down = 0;
+                }
+            } else if (arr[i] < arr[i - 1]) {
+                down++;
+                if (i < arr.length - 1 && arr[i] <= arr[i + 1]) {
+                    if (up > 0) length = up + down;
+                    if (length > maxLength) maxLength = length;
+                    up = 0;
+                } else {
+                    if (up > 0) length = up + down;
+                    if (length > maxLength) maxLength = length;
+                }
+            }
+        }
+        if (maxLength != 0) return maxLength + 1;
+        else return 0;
+    }
+
+    //https://leetcode.com/problems/find-pivot-index/submissions/962522716/
+    public int pivotIndex_724(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int sumLeft = 0;
+            int sumRight = 0;
+            if (i == 0) {
+                for (int j = i + 1; j < nums.length; j++) sumRight += nums[j];
+            } else if (i == nums.length - 1) {
+                for (int j = 0; j < i; j++) sumLeft += nums[j];
+            } else if (i > 0 && i < nums.length - 1) {
+                for (int jL = 0; jL < i; jL++) sumLeft += nums[jL];
+                for (int jR = i + 1; jR < nums.length; jR++) sumRight += nums[jR];
+            }
+            if (sumLeft == sumRight) return i;
+        }
+        return -1;
+    }
+
+    //https://leetcode.com/problems/truncate-sentence/submissions/962476540/
+    public String truncateSentence_1816(String s, int k) {
+        int index = 0;
+        s = s + ' ';
+        for (int i = 0; i < k; i++) {
+            index = s.indexOf(' ', index + 1);
+        }
+        return s.substring(0, index);
+    }
+
+    //https://leetcode.com/problems/reverse-string/submissions/962482310/
+    public void reverseString_344(char[] s) {
+        char replacement;
+        for (int i = 0; i < s.length / 2; i++) {
+            replacement = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = replacement;
+        }
+    }
+
+    //https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/submissions/962695500/
+    public int findMinArrowShots_452(int[][] points) {
+        Arrays.sort(points, (a, b) -> {
+            if(a[0] == b[0]) {
+                return Integer.compare(b[1], a[1]);
+            }
+            return Integer.compare(a[0], b[0]);
+        });
+        boolean[] hasBeenBurst = new boolean[points.length];
+        int numOfArrows = 0;
+        for (int i = 0; i < points.length; i++) {
+            if (!hasBeenBurst[i]) {
+                numOfArrows++;
+                hasBeenBurst[i]=true;
+                int leftBound = points[i][0];
+                int rightBound = points[i][1];
+                for (int j = i + 1; j < points.length; j++) {
+                    if (!hasBeenBurst[j] && rightBound >= points[j][0] && points[j][1] >= leftBound) {
+                        hasBeenBurst[j] = true;
+                        leftBound = Math.max(leftBound, points[j][0]);
+                        rightBound = Math.min(rightBound, points[j][1]);
+                    }
+                }
+            }
+        }
+        return numOfArrows;
+    }
+
 }
--- datastructurealgorithm/btvn/Main.java
+++ datastructurealgorithm/btvn/Main.java
@@ -3,10 +3,8 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
         BTVN_Buoi2 btvn_buoi2 = new BTVN_Buoi2();
-//        int[] myArray = {0,1,0,3,12};
-//        btvn_buoi2.moveZeroes_283(myArray);
-//        for (int n:myArray) System.out.print(myArray[n]);
-//        System.out.println(btvn_buoi2.firstUniqChar_387("aabb"));
-        System.out.println("aabb".substring(0,1));
+        int[] myArray = {0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0};
+        int[][] points = {{3,9},{7,12},{3,8},{6,8},{9,10},{2,9},{0,9},{3,9},{0,6},{2,8}};
+        System.out.println(btvn_buoi2.findMinArrowShots_452(points));
     }
 }
